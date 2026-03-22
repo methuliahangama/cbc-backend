b528321a1568cb8a0a68d1ddef2a70e0a003147f
methuliahangama <methuliahangama13@gmail.com>
2026-03-23 01:09:31 +0530

    order validation

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/order.js";
 import { isCustomer } from "./userController.js";
+import Product from "../models/product.js";
 
 export async function createOrder(req, res) {
 
@@ -26,6 +27,31 @@ export async function createOrder(req, res) {
             orderId = "CBC" + newNumber;
         }
         const newOrderData = req.body;
+
+        const newProductArray = [];
+
+        for (let i = 0; i < newOrderData.orderedItems.length; i++) {
+            const product = await Product.findOne({ productID: newOrderData.orderedItems[i].productID });
+
+            if (product == null) {
+                res.json({
+                    message: "Product with ID " + newOrderData.orderedItems[i].productID + " not found"
+                })
+                return;
+            }
+
+            newProductArray[i] = {
+                name: product.productName,
+                price: product.price,
+                quantity: newOrderData.orderedItems[i].quantity,
+                image: product.images[0]
+            };
+
+            console.log(newProductArray);
+
+            newOrderData.orderedItems = newProductArray;
+        }
+
         newOrderData.orderId = orderId;
         newOrderData.email = req.user.email;
 
