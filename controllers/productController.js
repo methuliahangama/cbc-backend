320ad1051fa5b3ced43cd62eaa0d4cc5adee0119
methuliahangama <methuliahangama13@gmail.com>
2026-03-22 20:43:14 +0530

    created productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,33 @@
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
 export function createProduct(req, res) {
-    
+
+    if(!isAdmin(req)) {
+        res.json({
+            message: "Only admins can add products"
+        })
+        return;
+    }
+
+    const newProductData = req.body;
+    const product = new Product(newProductData);
+
+    product.save().then(() => {
+        res.json({
+            message: "Product created successfully"
+        })
+    })
+        .catch((error) => {
+            res.json({
+                message: error
+            })
+        })
 }
+
+export function getProducts(req, res) {
+    Product.find().then((products) => {
+        res.json(products);
+    })
+}
+
