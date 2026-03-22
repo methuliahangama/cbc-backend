79938fd7751005c019b8feff75745c84528df907
methuliahangama <methuliahangama13@gmail.com>
2026-03-22 19:39:17 +0530

    Removed productRouter and student Router js files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
