95b9709e61d8a73059e77f46c2e767b2ab8afb64
methuliahangama <methuliahangama13@gmail.com>
2026-03-24 01:33:01 +0530

    install cors and add it

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import productRouter from './routes/productRouter.js';
 import orderRouter from './routes/orderRouter.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ dotenv.config();
 const app = express();
 
 const mongoUrl = process.env.MONGO_DB_URI;
+
+app.use(cors());
+
 mongoose.connect(mongoUrl, {})
 const connection = mongoose.connection;
 connection.once("open", () => {
